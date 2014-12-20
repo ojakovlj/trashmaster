e84f7345f06260e77721ec6a6c636ced9731f37f
ojakovlj <oleg.jakovljev@gmail.com>
2014-12-20 15:20:06 +0100

    Dodani activity i sredjena petljancija s google API

--- app/src/main/java/com/sincress/trashmaster/MenuActivity.java
+++ app/src/main/java/com/sincress/trashmaster/MenuActivity.java
@@ -1,17 +1,38 @@
 package com.sincress.trashmaster;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MenuActivity extends ActionBarActivity {
+
+    private Button launchMap, launchOverview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        launchMap = (Button) findViewById(R.id.launch_map);
+        launchOverview = (Button) findViewById(R.id.launch_overview);
+        launchMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        launchOverview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, OverviewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
